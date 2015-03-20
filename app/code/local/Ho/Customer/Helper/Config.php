e47e123da5385b8783daf329e4f2150c364f1a75
Maikel Koek <maikel@h-o.nl>
2015-03-20 16:17:02 +0100

    [HO-CUS-FOL] B: Fixed email sender and template XML config paths

--- app/code/local/Ho/Customer/Helper/Config.php
+++ app/code/local/Ho/Customer/Helper/Config.php
@@ -5,8 +5,8 @@ class Ho_Customer_Helper_Config extends Mage_Core_Helper_Abstract
     const XML_PATH_COMPLETE_PROFILE_EMAIL_ENABLED   = 'ho_customer/complete_profile/enabled';
     const XML_PATH_COMPLETE_PROFILE_START_DATE      = 'ho_customer/complete_profile/start_date';
     const XML_PATH_COMPLETE_PROFILE_DAYS_AFTER      = 'ho_customer/complete_profile/days_after';
-    const XML_PATH_COMPLETE_PROFILE_SENDER          = 'ho_customer/complete_profile/sender';
-    const XML_PATH_COMPLETE_PROFILE_TEMPLATE        = 'ho_customer/complete_profile/template';
+    const XML_PATH_COMPLETE_PROFILE_SENDER          = 'ho_customer/complete_profile/email_sender';
+    const XML_PATH_COMPLETE_PROFILE_TEMPLATE        = 'ho_customer/complete_profile/email_template';
 
     /**
      * @param null|Mage_Core_Model_Store|int $store
