e415d60187c98a8053e0239983e96b2e8d5314d9
Maikel Koek <maikel@h-o.nl>
2015-03-20 15:10:06 +0100

    [HO-CUS-FOL] B: Fixed config XML paths

--- app/code/local/Ho/Customer/Helper/Config.php
+++ app/code/local/Ho/Customer/Helper/Config.php
@@ -2,11 +2,11 @@
 
 class Ho_Customer_Helper_Config extends Mage_Core_Helper_Abstract
 {
-    const XML_PATH_COMPLETE_PROFILE_EMAIL_ENABLED   = 'ho_customer/follow_up_email/enabled';
-    const XML_PATH_COMPLETE_PROFILE_START_DATE      = 'ho_customer/follow_up_email/start_date';
-    const XML_PATH_COMPLETE_PROFILE_DAYS_AFTER      = 'ho_customer/follow_up_email/days_after';
-    const XML_PATH_COMPLETE_PROFILE_SENDER          = 'ho_customer/follow_up_email/sender';
-    const XML_PATH_COMPLETE_PROFILE_TEMPLATE        = 'ho_customer/follow_up_email/template';
+    const XML_PATH_COMPLETE_PROFILE_EMAIL_ENABLED   = 'ho_customer/complete_profile/enabled';
+    const XML_PATH_COMPLETE_PROFILE_START_DATE      = 'ho_customer/complete_profile/start_date';
+    const XML_PATH_COMPLETE_PROFILE_DAYS_AFTER      = 'ho_customer/complete_profile/days_after';
+    const XML_PATH_COMPLETE_PROFILE_SENDER          = 'ho_customer/complete_profile/sender';
+    const XML_PATH_COMPLETE_PROFILE_TEMPLATE        = 'ho_customer/complete_profile/template';
 
     /**
      * @param null|Mage_Core_Model_Store|int $store
