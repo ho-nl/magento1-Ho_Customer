f152fd04d68a928d22a2ade1ba0184fbabf1fef3
Maikel Koek <maikel@h-o.nl>
2015-03-23 13:45:12 +0100

    [HO-CUS-FOL] +: Gender and DoB to complete-profile form, styling, form working

--- app/code/local/Ho/Customer/Block/CompleteProfile.php
+++ app/code/local/Ho/Customer/Block/CompleteProfile.php
@@ -2,4 +2,11 @@
 
 class Ho_Customer_Block_CompleteProfile extends Mage_Core_Block_Template
 {
+    /**
+     * @return Mage_Customer_Model_Customer
+     */
+    public function getCustomer()
+    {
+        return Mage::getSingleton('customer/session')->getCustomer();
+    }
 }
