e02736195b4f562b1d0471d30e68fea2a99cf21d
Paul Hachmang <paul@h-o.nl>
2015-07-22 16:35:00 +0200

    Use a distinct query to return the labels for the customer sections.

--- app/code/local/Ho/Customer/Helper/Customer/Customer.php
+++ app/code/local/Ho/Customer/Helper/Customer/Customer.php
@@ -2,36 +2,43 @@
 
 class Ho_Customer_Helper_Customer_Customer extends Enterprise_Customer_Helper_Customer
 {
+    protected $_options;
     /**
      * Return available customer attribute form as select options
-     *
-     * Extended to add the 'Complete Profile' option
+     * Extended to add the Admin Medical Information customer attribute type
      *
      * @return array
      */
     public function getAttributeFormOptions()
     {
-        return array(
-            array(
-                'label' => Mage::helper('enterprise_customer')->__('Customer Checkout Register'),
-                'value' => 'checkout_register'
-            ),
-            array(
-                'label' => Mage::helper('enterprise_customer')->__('Customer Registration'),
-                'value' => 'customer_account_create'
-            ),
-            array(
-                'label' => Mage::helper('enterprise_customer')->__('Customer Account Edit'),
-                'value' => 'customer_account_edit'
-            ),
-            array(
-                'label' => Mage::helper('enterprise_customer')->__('Admin Checkout'),
-                'value' => 'adminhtml_checkout'
-            ),
-            array(
-                'label' => Mage::helper('ho_customer')->__('Complete Profile'),
-                'value' => 'ho_customer_complete_profile',
-            ),
-        );
+        if (! is_null($this->_options)) {
+            return $this->_options;
+        }
+
+
+        /** @var Mage_Core_Model_Resource $resource */
+        $resource = Mage::getSingleton('core/resource');
+
+
+        /** @var Magento_Db_Adapter_Pdo_Mysql $writeConnection */
+        $writeConnection = $resource->getConnection('core_read');
+
+        $select = $writeConnection->select();
+        $select->from($resource->getTableName('customer/form_attribute'), [
+            'form_code' => new Zend_Db_Expr('DISTINCT form_code')
+        ]);
+
+        $formCodes = $writeConnection->fetchCol($select);
+        $this->_options = [];
+        foreach ($formCodes as $formCode) {
+            $formLabel = ucwords(str_replace(['_', 'adminhtml'], [' ', 'admin'], $formCode));
+
+            $this->_options[] = [
+                'label' => Mage::helper('enterprise_customer')->__($formLabel),
+                'value' => $formCode
+            ];
+        }
+
+        return $this->_options;
     }
 }
