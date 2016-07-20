d13885ada138edf841e7f7254131b9b055a8dcc7
Maikel Koek <maikel@h-o.nl>
2016-07-20 11:38:01 +0200

    [SHOP-407] +: Configuration for discount when subscribing to newsletter in checkout

--- app/code/local/Ho/Customer/Helper/Config.php
+++ app/code/local/Ho/Customer/Helper/Config.php
@@ -11,6 +11,11 @@ class Ho_Customer_Helper_Config extends Mage_Core_Helper_Abstract
     const XML_PATH_COMPLETE_PROFILE_DISCOUNT_ENABLED    = 'ho_customer/complete_profile/discount_enabled';
     const XML_PATH_COMPLETE_PROFILE_DISCOUNT_RULE_ID    = 'ho_customer/complete_profile/discount_rule_id';
 
+    const XML_PATH_NEWSLETTER_DISCOUNT_ENABLED          = 'ho_customer/newsletter_discount/enabled';
+    const XML_PATH_NEWSLETTER_DISCOUNT_PRICE_RULE       = 'ho_customer/newsletter_discount/price_rule';
+    const XML_PATH_NEWSLETTER_DISCOUNT_EMAIL_TEMPLATE   = 'ho_customer/newsletter_discount/email_template';
+    const XML_PATH_NEWSLETTER_DISCOUNT_EMAIL_SENDER     = 'ho_customer/newsletter_discount/email_sender';
+
     /**
      * @param null|Mage_Core_Model_Store|int $store
      * @return bool
@@ -72,5 +77,41 @@ class Ho_Customer_Helper_Config extends Mage_Core_Helper_Abstract
     public function getDiscountRuleId($store = null)
     {
         return Mage::getStoreConfig(self::XML_PATH_COMPLETE_PROFILE_DISCOUNT_RULE_ID, $store);
+    }
+
+    /**
+     * @param null|Mage_Core_Model_Store|int $store
+     * @return bool
+     */
+    public function getNewsletterDiscountEnabled($store = null)
+    {
+        return Mage::getStoreConfigFlag(self::XML_PATH_NEWSLETTER_DISCOUNT_ENABLED, $store);
+    }
+
+    /**
+     * @param null|Mage_Core_Model_Store|int $store
+     * @return string
+     */
+    public function getNewsletterDiscountPriceRule($store = null)
+    {
+        return Mage::getStoreConfig(self::XML_PATH_NEWSLETTER_DISCOUNT_PRICE_RULE, $store);
+    }
+
+    /**
+     * @param null|Mage_Core_Model_Store|int $store
+     * @return string
+     */
+    public function getNewsletterDiscountEmailTemplate($store = null)
+    {
+        return Mage::getStoreConfig(self::XML_PATH_NEWSLETTER_DISCOUNT_EMAIL_TEMPLATE, $store);
+    }
+
+    /**
+     * @param null|Mage_Core_Model_Store|int $store
+     * @return string
+     */
+    public function getNewsletterDiscountEmailSender($store = null)
+    {
+        return Mage::getStoreConfig(self::XML_PATH_NEWSLETTER_DISCOUNT_EMAIL_SENDER, $store);
     }
 }
